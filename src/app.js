fc2149a41def9f6f2f8536405120b19a27092134
Aisyhnabilaaa <aisyarahmawati322@gmail.com>
2025-05-22 12:14:08 +0800

    deploy ke vercel

--- src/app.js
+++ src/app.js
@@ -9,9 +9,9 @@ app.use(cors());
 app.get("/", (req, res) => {
 res.send('Hello there!')
 })
-// app.listen(3000, () => {
-// console.log(`App listening on port ` + 3000)
-// })
+app.listen(3000, () => {
+console.log(`App listening on port ` + 3000)
+})
 
 const tkdRouter = require("./tkd/tkd.controller");
 app.use("/api/tkd", tkdRouter);
@@ -22,4 +22,4 @@ app.use('/api/alokasi', alokasiController);
 const adminController = require('./adminlogin/admin.controller');
 app.use('/api/admin', adminController);
 
-export default app
+// export default app
