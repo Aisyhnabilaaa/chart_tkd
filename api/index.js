683b489cbb97cb96b84f54f020a5078b8a28f6ef
Aisyhnabilaaa <aisyarahmawati322@gmail.com>
2025-05-22 14:49:21 +0800

    mau deploy ke vercel

--- api/index.js
+++ api/index.js
@@ -1,4 +1,24 @@
+const express = require('express');
 const serverless = require('serverless-http');
-const app = require('../src/app');
+const cors = require('cors');
 
-module.exports = serverless(app);
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Import router dari folder lain
+const tkdRouter = require('../tkd/tkd.controller');
+const alokasiController = require('../AlokasiTKD/alokasi.controller');
+const adminController = require('../adminlogin/admin.controller');
+
+app.get('/', (req, res) => {
+  res.send('Hello from Vercel serverless Express!');
+});
+
+app.use('/api/tkd', tkdRouter);
+app.use('/api/alokasi', alokasiController);
+app.use('/api/admin', adminController);
+
+// Export handler buat Vercel
+module.exports.handler = serverless(app);
