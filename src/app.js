1ebe32fa776a23721963d85cd11e63ee797cd53d
Aisyhnabilaaa <aisyarahmawati322@gmail.com>
2025-05-23 00:22:18 +0800

    mau deploy ke vercel

--- src/app.js
+++ src/app.js
@@ -1,17 +1,39 @@
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
+// app.use(express.json());
+
+// const cors = require("cors");
+// app.use(cors());
+
+
+// app.get("/", (req, res) => {
+// res.send('Hello there!')
+// })
+// // app.listen(3000, () => {
+// // console.log(`App listening on port ` + 3000)
+// // })
+
+// const tkdRouter = require("./tkd/tkd.controller");
+// app.use("/tkd", tkdRouter);
+
+// const alokasiController = require('./AlokasiTKD/alokasi.controller');
+// app.use('/alokasi', alokasiController);
+
+// const adminController = require('./adminlogin/admin.controller');
+// app.use('/admin', adminController);
+
+// module.exports = app;
+
+const express = require('express');
+const app = express();
 app.use(express.json());
 
 const cors = require("cors");
 app.use(cors());
 
-
 app.get("/", (req, res) => {
-res.send('Hello there!')
-})
-// app.listen(3000, () => {
-// console.log(`App listening on port ` + 3000)
-// })
+  res.send('Hello there!');
+});
 
 const tkdRouter = require("./tkd/tkd.controller");
 app.use("/tkd", tkdRouter);
