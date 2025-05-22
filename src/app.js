330a68045a36ace1ad9e87e4e4e163a30fffc8fb
Aisyhnabilaaa <aisyarahmawati322@gmail.com>
2025-05-22 14:30:12 +0800

    mau deploy ke vercel

--- src/app.js
+++ src/app.js
@@ -1,25 +1,70 @@
-const express = require('express')
-const app = express()
-app.use(express.json());
+// const express = require('express')
+// const app = express()
+// app.use(express.json());
 
-const cors = require("cors");
+// const cors = require("cors");
+// app.use(cors());
+
+
+// app.get("/", (req, res) => {
+// res.send('Hello there!')
+// })
+// // app.listen(3000, () => {
+// // console.log(`App listening on port ` + 3000)
+// // })
+
+// const tkdRouter = require("./tkd/tkd.controller");
+// app.use("/api/tkd", tkdRouter);
+
+// const alokasiController = require('./AlokasiTKD/alokasi.controller');
+// app.use('/api/alokasi', alokasiController);
+
+// const adminController = require('./adminlogin/admin.controller');
+// app.use('/api/admin', adminController);
+
+// // export default app
+
+// const express = require('express');
+// const cors = require('cors');
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.get("/", (req, res) => {
+//   res.send('Hello there!');
+// });
+
+// const tkdRouter = require("./tkd/tkd.controller");
+// app.use("/api/tkd", tkdRouter);
+
+// const alokasiController = require('./AlokasiTKD/alokasi.controller');
+// app.use('/api/alokasi', alokasiController);
+
+// const adminController = require('./adminlogin/admin.controller');
+// app.use('/api/admin', adminController);
+
+// module.exports = app;
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
 app.use(cors());
 
-
 app.get("/", (req, res) => {
-res.send('Hello there!')
-})
-// app.listen(3000, () => {
-// console.log(`App listening on port ` + 3000)
-// })
+  res.send("Hello there!");
+});
 
-const tkdRouter = require("./tkd/tkd.controller");
+import tkdRouter from './tkd/tkd.controller.js';
 app.use("/api/tkd", tkdRouter);
 
-const alokasiController = require('./AlokasiTKD/alokasi.controller');
-app.use('/api/alokasi', alokasiController);
+import alokasiController from './AlokasiTKD/alokasi.controller.js';
+app.use("/api/alokasi", alokasiController);
 
-const adminController = require('./adminlogin/admin.controller');
-app.use('/api/admin', adminController);
+import adminController from './adminlogin/admin.controller.js';
+app.use("/api/admin", adminController);
 
-// export default app
+export default app;
+
