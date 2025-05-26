b7afe2cc41fb815b3155a2ee631d5e3e34cce810
Aisyhnabilaaa <aisyarahmawati322@gmail.com>
2025-05-26 10:11:35 +0800

    fixed railway

--- src/app.js
+++ src/app.js
@@ -5,58 +5,19 @@ app.use(express.json());
 const cors = require("cors");
 app.use(cors());
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.get("/", (req, res) => {
 res.send('Hello there!')
+})
+app.listen(3000, () => {
+console.log(`App listening on port ` + 3000)
 })
 
 const tkdRouter = require("./tkd/tkd.controller");
 app.use("/tkd", tkdRouter);
 
 const alokasiController = require('./AlokasiTKD/alokasi.controller');
-app.use('/alokasi', alokasiController);
+app.use('/api/alokasi', alokasiController);
 
 const adminController = require('./adminlogin/admin.controller');
 app.use('/admin', adminController);
-
-
-
-// const express = require('express');
-// const cors = require('cors');
-// const app = express();
-
-// const allowedOrigins = [
-//   "https://tkdkppnpalu-production.up.railway.app",
-//   "https://adminkppnpalu-production.up.railway.app"
-// ];
-
-// const corsOptions = {
-//   origin: function(origin, callback){
-//     if(!origin) return callback(null, true);
-//     if(allowedOrigins.includes(origin)){
-//       callback(null, true);
-//     } else {
-//       callback(new Error('Origin not allowed by CORS'));
-//     }
-//   },
-//   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-//   allowedHeaders: ["Content-Type", "Authorization"],
-//   credentials: true
-// };
-
-// app.use(cors(corsOptions));
-// app.options('*', cors(corsOptions)); // <-- Penting untuk preflight
-
-// app.use(express.json());
-
-// // Route admin
-// app.post('/admin/login', (req, res) => {
-//   // login logic
-//   res.json({ success: true });
-// });
-
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
