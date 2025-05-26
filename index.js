e262302080baf0258fa6659200547ad872bd4dcb
Aisyhnabilaaa <aisyarahmawati322@gmail.com>
2025-05-26 09:54:03 +0800

    railway selesai

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const serverless = require("serverless-http");
-const app = require("./src/app");
+// const serverless = require("serverless-http");
+// const app = require("./src/app");
 
-module.exports = {
-  config: {
-    runtime: "nodejs22.x" // atau "nodejs20.x" tergantung kebutuhan
-  },
-  handler: serverless(app)
-};
+// module.exports = {
+//   config: {
+//     runtime: "nodejs22.x" // atau "nodejs20.x" tergantung kebutuhan
+//   },
+//   handler: serverless(app)
+// };
