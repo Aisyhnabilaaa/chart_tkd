cfb73a135d9d756b544994c8daf5ccd62d59feee
Aisyhnabilaaa <aisyarahmawati322@gmail.com>
2025-05-26 10:24:42 +0800

    fix railway

--- src/app.js
+++ src/app.js
@@ -13,11 +13,15 @@ app.listen(3000, () => {
 console.log(`App listening on port ` + 3000)
 })
 
+// Import routes after basic middleware
 const tkdRouter = require("./tkd/tkd.controller");
+console.log("Loaded tkdRouter");
 app.use("/tkd", tkdRouter);
 
 const alokasiController = require('./AlokasiTKD/alokasi.controller');
-app.use('/api/alokasi', alokasiController);
+console.log("Loaded alokasiController");
+app.use('/alokasi', alokasiController);
 
 const adminController = require('./adminlogin/admin.controller');
+console.log("Loaded adminController");
 app.use('/admin', adminController);
